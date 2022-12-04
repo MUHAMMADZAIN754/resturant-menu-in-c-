23ec17d4304627be7a4dedf294109e6cba941525
MUHAMMAD ZAIN <111556373+MUHAMMADZAIN754@users.noreply.github.com>
2022-12-04 08:28:48 -0800

    second change

--- firstproject.cpp
+++ firstproject.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 using namespace std;
-int main() {
-    std::cout << "Hello World!";
+int main() 
+{
+float a,b;
+double divide;
+a=37;
+b=17;
+divide=37/17;
+cout<<"the value of division is "<<divide<<endl;
+cout << "Hello World!";
     cout<<"don't left you will suceed one day"<<endl;
+    cout<< " i am zain"<<endl;
+  
     return 0;
 }
